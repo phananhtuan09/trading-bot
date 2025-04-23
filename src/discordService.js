1687364ca349370c6a114bd47d88783352874145
phananhtuan <tuanp6905@gmail.com>
2025-04-23 16:58:41 +0700

    update message discord

--- src/discordService.js
+++ src/discordService.js
@@ -5,13 +5,26 @@ const username = 'Crypto Trading Bot'
 
 // Tạo Embed message cho tín hiệu giao dịch
 function createSignalEmbed(signal) {
-  const futures = signal.futuresDetails || {}
+  const futuresDetails = {
+    strategies: '',
+    strength: '',
+  }
+  Object.values(signal.futuresDetails).forEach((group) => {
+    if (group.direction === signal.decision) {
+      const strategies = group.contributors.sort().join(', ')
+      futuresDetails.strategies = strategies
+      futuresDetails.strength = group.strength
+    }
+  })
   return {
     title: `Tín hiệu: ${signal.symbol}`,
     description:
-      `**Hành động:** ${futures.direction}\n` +
+      `**Hành động:** ${signal.decision || 'N/A'}\n` +
       `**Giá hiện tại:** ${signal.price}\n` +
-      `**Chiến lược:** ${signal.strategy}\n`,
+      `**Độ mạnh:** ${futuresDetails.strength || 'N/A'}\n` +
+      `**TP(ROI %):** ${signal.TP_ROI.toFixed(4) || 'N/A'}\n` +
+      `**SL(ROI %):** ${signal.SL_ROI.toFixed(4) || 'N/A'}\n` +
+      `**Chiến lược:** ${futuresDetails.strategies || 'N/A'}`,
     timestamp: new Date().toISOString(),
     footer: {
       text: username,
